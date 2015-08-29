049b66016f7c9a56abdf949cb569c2b62f6db46c
neelp463 <flamezillah@gmail.com>
2015-08-28 23:01:34 -0400

    Improved calculate plate function

--- plates/plate.js
+++ plates/plate.js
@@ -1,65 +1,67 @@
 function calcPlates() {
-    document.getElementById("txt").innerHTML = "";
-    document.getElementById("FF").innerHTML = "";
-    document.getElementById("TF").innerHTML = "";
-    document.getElementById("T").innerHTML = "";
-    document.getElementById("F").innerHTML = "";
-    document.getElementById("TH").innerHTML = "";
-
-    var input = document.getElementById("weight");
-    var weight = input.value;
-    if((weight % 5) != 0) {
-        document.getElementById("txt").innerHTML = "Invalid Weight";
+    $("#plates").empty();
+    $("#output").empty();
+    var plates = [0, 0, 0, 0, 0];
+    var input = $("#weight").val();
+    if((input % 5) != 0) {
+        $("#output").html("Invalid Weight");
         return;
     }
-    if(weight < 45) {
-        document.getElementById("txt").innerHTML = "Invalid Weight";
+    if(input < 45) {
+        $("#output").html("Invalid Weight");
         return;
     }
-    if(weight == 45) {
-        document.getElementById("txt").innerHTML = "Empty Bar";
+    if(input == 45) {
+        $("#output").html("Empty Bar");
         return;
     }
-    var FF = 0, TF = 0, T = 0, F = 0, TH = 0;
-    var weightLeft = weight-45;
+    var weightLeft = input - 45;
     while(weightLeft != 0) {
         if(weightLeft >= 90) {
             weightLeft -= 90;
-            FF++;
+            plates[0]++;
             continue;
             //console.log("add 45lb plate");
         }
         if(weightLeft >= 50) {
             weightLeft -= 50;
-            TF++;
+            plates[1]++;
             continue;
             //console.log("add 25lb plate");
         }
         if(weightLeft >= 20) {
             weightLeft -= 20;
-            T++;
+            plates[2]++;
             continue;
             //console.log("add 10lb plate");
         }
         if(weightLeft >= 10) {
             weightLeft -= 10;
-            F++
+            plates[3]++;
             continue;
             //console.log("add 5lb plate");
         }
         if(weightLeft >= 5) {
             weightLeft -= 5;
-            TH++
+            plates[4]++;
             continue;
             //console.log("add 2.5lb plate");
         }
     }
-    var output = document.getElementById("output");
-    document.getElementById("txt").innerHTML = "Add these plates on each side:";
-    console.log(FF, TF, T, F, TH);
-    if(FF>0) { document.getElementById("FF").innerHTML = FF + "x " + "45lb"; }
-    if(TF>0) { document.getElementById("TF").innerHTML = TF + "x " + "25lb"; }
-    if(T>0) { document.getElementById("T").innerHTML = T + "x " + "10lb"; }
-    if(F>0) { document.getElementById("F").innerHTML = F + "x " + "5lb"; }
-    if(TH>0) { document.getElementById("TH").innerHTML = TH + "x " + "2.5lb"; }
+    $("#output").html("Add these plates on each side:");
+    if(plates[0] > 0) {
+        $("#plates").append("<li>" + plates[0] + "x 45lb plate" + "</li>");   
+    }
+    if(plates[1] > 0) {
+        $("#plates").append("<li>" + plates[1] + "x 25lb plate" + "</li>");   
+    }
+    if(plates[2] > 0) {
+        $("#plates").append("<li>" + plates[2] + "x 10b plate" + "</li>");   
+    }
+    if(plates[3] > 0) {
+        $("#plates").append("<li>" + plates[3] + "x 5lb plate" + "</li>");   
+    }
+    if(plates[4] > 0) {
+        $("#plates").append("<li>" + plates[4] + "x 2.5lb plate" + "</li>");   
+    }
 }
